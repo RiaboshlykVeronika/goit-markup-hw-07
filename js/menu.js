96adcedab3d59b0147cae7331e658d51996ea583
RiaboshlykVeronika <riaboshlyk.veronika@gmail.com>
2021-07-10 01:21:15 +0200

    add code
    add code

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openMenulBtn: document.querySelector('[.menu-open]'),
-    closeMenuBtn: document.querySelector('[.menu-close]'),
-    menu: document.querySelector('[.mobile-nav]'),
+    openMenuBtn: document.querySelector('.menu-open'),
+    closeMenuBtn: document.querySelector('.menu-close'),
+    menu: document.querySelector('.mobile-nav'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.medu.classList.toggle('is-hidden');
+    refs.menu.classList.toggle('is-hidden');
   }
 })();
